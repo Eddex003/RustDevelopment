b83c663fdb53f4dc864c24c315542ac4dfebc351
Eddex003 <mariosup3r99@gmail.com>
2026-03-02 04:55:19 +0000

    Commiting for Word Frequency Counter Assignment

--- word_counter/src/wordCounter.rs
+++ word_counter/src/wordCounter.rs
@@ -31,4 +31,4 @@ fn main() {
     let text = "the quick brown fox jumps over the lazy dog the quick brown fox";
     let (word, count) = most_frequent_word(text);
     println!("Most frequent word: \"{}\" ({} times)", word, count);
-} // Changing
+}
